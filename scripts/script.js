cd61319a1621223393b7fdf88415d57a5da06d4f
Yaroslav <yaroslavtereshchuk@gmail.com>
2018-11-03 23:02:58 +0200

    init

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+document.addEventListener(
+  "click",
+  function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+      text = target.textContent || target.innerText;
+    console.log(target);
+  },
+  false
+);

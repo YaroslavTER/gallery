6c21ab7839c352ff011736beba246f4f4ba41ee2
Yaroslav <yaroslavtereshchuk@gmail.com>
2018-11-09 23:42:33 +0200

    added script to modal vindow display

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,35 @@
-document.addEventListener(
-  "click",
-  function(e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement,
-      text = target.textContent || target.innerText;
-    console.log(target);
-  },
-  false
-);
+const setDisplayToElement = (className, displayValue) => {
+  const modalStyles = document.getElementsByClassName(className);
+  [...modalStyles].forEach(element => {
+    element.style.display = displayValue;
+  });
+};
+
+const setDisplayOnElementEvent = (
+  elements,
+  elementEvent,
+  className,
+  display
+) => {
+  [...elements].forEach(element =>
+    element.addEventListener(elementEvent, event => {
+      const target = event.target;
+      const attributes = target.attributes;
+      if (attributes.class.value === className) {
+        setDisplayToElement(display.setValueTo, display.value);
+      }
+    })
+  );
+};
+
+const postsWrapper = document.getElementsByClassName("posts-wrapper");
+const close = document.getElementsByClassName("close");
+
+setDisplayOnElementEvent(postsWrapper, "click", "post", {
+  setValueTo: "modal-wrapper",
+  value: "grid"
+});
+setDisplayOnElementEvent(close, "click", "close", {
+  setValueTo: "modal-wrapper",
+  value: "none"
+});

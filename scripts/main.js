eb325defa8222e71729f5db84cac1894e5b69c75
Yaroslav <yaroslavtereshchuk@gmail.com>
2018-11-11 19:42:05 +0200

    created about us page html, css and upgraded visibility script

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,60 @@
-const setDisplayToElement = (className, displayValue) => {
-  const modalStyles = document.getElementsByClassName(className);
-  [...modalStyles].forEach(element => {
-    element.style.display = displayValue;
-  });
-};
+function Visibility() {
+  const setDisplayToElement = (className, displayValue) => {
+    const modalStyles = document.getElementsByClassName(className);
+    [...modalStyles].forEach(element => {
+      element.style.display = displayValue;
+    });
+  };
 
-const setDisplayOnElementEvent = (
-  elements,
-  elementEvent,
-  className,
-  display
-) => {
-  [...elements].forEach(element =>
-    element.addEventListener(elementEvent, event => {
-      const target = event.target;
-      const attributes = target.attributes;
-      if (attributes.class.value === className) {
-        setDisplayToElement(display.setValueTo, display.value);
-      }
-    })
-  );
-};
+  const setDisplayOnElementEvent = (
+    elements,
+    elementEvent,
+    className,
+    display
+  ) => {
+    [...elements].forEach(element => {
+      element.addEventListener(elementEvent, event => {
+        const target = event.target;
+        const attributes = target.attributes;
+        if (attributes.class.value === className) {
+          setDisplayToElement(display.setValueTo, display.value);
+        }
+      });
+    });
+  };
 
-const postsWrapper = document.getElementsByClassName("posts-wrapper");
-const close = document.getElementsByClassName("close");
+  this.apply = array => {
+    array.forEach(element => {
+      const domElement = document.getElementsByClassName(
+        element.domElementClass
+      );
+      setDisplayOnElementEvent(domElement, element.event, element.className, {
+        setValueTo: element.displayValue.setValueTo,
+        value: element.displayValue.value
+      });
+    });
+  };
+}
 
-setDisplayOnElementEvent(postsWrapper, "click", "post", {
-  setValueTo: "modal-wrapper",
-  value: "grid"
-});
-setDisplayOnElementEvent(close, "click", "close", {
-  setValueTo: "modal-wrapper",
-  value: "none"
-});
+const visibility = new Visibility();
+
+visibility.apply([
+  {
+    domElementClass: "posts-wrapper",
+    event: "click",
+    className: "post",
+    displayValue: { setValueTo: "modal-wrapper", value: "grid" }
+  },
+  {
+    domElementClass: "close",
+    event: "click",
+    className: "close",
+    displayValue: { setValueTo: "modal-wrapper", value: "none" }
+  },
+  {
+    domElementClass: "modal-wrapper",
+    event: "click",
+    className: "modal-wrapper",
+    displayValue: { setValueTo: "modal-wrapper", value: "none" }
+  }
+]);
